3c628553ec5f50f33d357fdf2c680b8cdeb170f4
Hossein <hossein.saafi@gmail.com>
2023-09-28 15:50:26 -0700

    exercise 3

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -6,3 +6,13 @@ puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+@store3 = Store.find(3)
+
+if @store3
+  @store3.destory
+  puts "Store with id 3 has been deleted."
+else
+  puts "Store with id 3 not found."
+end
+
+puts "Number of stores in the database: #{Store.count}"
