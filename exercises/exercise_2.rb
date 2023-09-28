8fdc3dd2f2dbc1e624fd3b47cb49526f79712b34
Hossein <hossein.saafi@gmail.com>
2023-09-28 15:45:41 -0700

    exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,12 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find(1)
+@store2 = Store.find(2)
+if @store1
+  @store1.update(name: "Happy Store")  
+  @store1.save 
+else
+  puts "Store with id 1 not found."
+end
+
